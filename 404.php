8c882088a0c7dcbd650fe11b206c0d1379116f5f
moritzgvt <hello@moritzgut.de>
2026-02-28 06:44:02 +0000

    Properly style not found page

--- 404.php
+++ 404.php
@@ -1,8 +1,17 @@
 <?php get_header(); ?>
 
-  <div>
-    <h1>404</h1>
-    <p>Page not found</p>
+<section class="grid module">
+  <div class="subgrid not-found">
+    <div class="card col-w4p2 background-dark">
+      <div class="textbox">
+        <h1>404 - Not Found</h1>
+        <p><?php _e('Die Seite konnte nicht gefunden werden.', 'Theme'); ?></p>
+      </div>
+      <div class="buttonbox">
+        <a href="<?php echo home_url(); ?>" class="button button-primary"><?php _e('Zur Startseite', 'Theme'); ?></a>
+      </div>
+    </div>
   </div>
+</section>
 
 <?php get_footer(); ?>
